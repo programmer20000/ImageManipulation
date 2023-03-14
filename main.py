2c1ba79fa4ace3700fe798e6b52197fa0ec252c2
Mihai Apostol <mihaiapostol153@gmail.com>
2023-03-14 16:12:47 +0200

    bug fix in method create_image

--- main.py
+++ main.py
@@ -16,24 +16,28 @@ class ImageManipulator:
         except ValueError:
             return 'incorrect path to image please check path'
 
+    def create_folder(self,name_folder):
+        if not os.path.exists(name_folder):
+            os.mkdir(name_folder)
+
     def resize_image(self):
-        if not os.path.exists('new_image'):
-            os.mkdir('new_image')
+        self.create_folder('new_image')
 
         self.select_image()
         try:
-            self.size = map(int,input('Please enter width and height:\n').split(' '))
+            self.new_size_image = map(int, input('Please enter width and height:\n').split(' '))
             self.new_name_image = input('Please new name image: \n')
-            self.new_image = self.path_image.resize(size=tuple(self.size))
+            self.new_image = self.path_image.resize(size=tuple(self.new_size_image))
             self.new_image.save(f'new_image/{self.new_name_image}')
 
         except ValueError:
             print('Please enter correct dates')
 
     def create_image(self, mode: str = 'RGB', size: tuple = (100, 100), color='blue', name='image.jpg'):
-        """this inputs are for  query user  parameters for create image"""
+        """this method is for created images"""
+        self.create_folder('created_images')
         self.image = Image.new(mode=mode, size=size, color=color)
-        self.image.save(name)
+        self.image.save(f'created_images/{name}')
 
     def select_variance(self):
         self.variance = input('Please select variance: 1:[resize image] 2:[create image] 3:[exit program]\n')
@@ -42,7 +46,12 @@ class ImageManipulator:
             case '1':
                 return self.resize_image()
             case '2':
-                ...
+                self.color_mode = input('Please enter color mode it can be RGB:\n')
+                self.new_size = map(int, input('Please enter width and height:\n').split(' '))
+                self.color = map(int, input('Please enter color:\n').split(' '))
+                self.name = input('Please enter name:\n')
+                self.create_image(mode=self.color_mode, size=tuple(self.new_size), color=tuple(self.color),
+                                  name=self.name)
             case '3':
                 self.exit_program()
             case _:
